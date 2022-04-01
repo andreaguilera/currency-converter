ec7da62eca162253d6b911498034b886e8bcfe7d
andreaguilera <andre.aguilera55@hotmail.com>
2022-03-31 21:01:03 -0300

    change from axios to fetch

--- currency-converter/src/services/api.ts
+++ currency-converter/src/services/api.ts
@@ -1,17 +1,8 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://v6.exchangerate-api.com/v6/20900fc28746b4d4f2450b91/",
-});
+const BASE_URL = "https://v6.exchangerate-api.com/v6/20900fc28746b4d4f2450b91/";
 
 export const getCurrencies = async (): Promise<any> => {
-  const response = await api.get("latest", {
-    headers: {
-      "Access-Control-Allow-Origin": "http://localhost:3000",
-    },
-  });
-  console.log(response.data);
-  return response.data.currencies;
+  const response = await fetch(`${BASE_URL}latest/USD`);
+  const data = await response.json();
+  console.log(data.conversion_rates);
+  return data.conversion_rates;
 };
-
-export default api;
